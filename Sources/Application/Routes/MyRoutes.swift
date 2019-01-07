f6475b8a9f5f20770ff32fc6187d1318ea902e15
Chris Bailey <baileyc@uk.ibm.com>
2019-01-07 17:50:09 +0000

    Update MyRoutes.swift

--- Sources/Application/Routes/MyRoutes.swift
+++ Sources/Application/Routes/MyRoutes.swift
@@ -2,25 +2,23 @@ import LoggerAPI
 import Health
 import KituraContracts
 
-func initializeMyRoutes(app: App) {
-    
+func initializeMyRoutes(app: App) {  
     app.router.get("/myroute", handler: getStatus)
     app.router.get("/mysecureroute", handler: getStatusSecure)
-    
-    private func getStatus(respondWith: @escaping (Status?, RequestError?) -> Void) -> Void {
-        if health.status.state == .UP {
-            respondWith(health.status, nil)
-        } else {
-            respondWith(nil, RequestError(.serviceUnavailable, body: health.status))
-        }
+}
+
+func getStatus(respondWith: @escaping (Status?, RequestError?) -> Void) -> Void {
+    if health.status.state == .UP {
+        respondWith(health.status, nil)
+    } else {
+        respondWith(nil, RequestError(.serviceUnavailable, body: health.status))
     }
-    
-    private func getStatusSecure(user: UserAuth, respondWith: @escaping (Status?, RequestError?) -> Void) -> Void {
-        if health.status.state == .UP {
-            respondWith(health.status, nil)
-        } else {
-            respondWith(nil, RequestError(.serviceUnavailable, body: health.status))
-        }
+}
+   
+func getStatusSecure(user: UserAuth, respondWith: @escaping (Status?, RequestError?) -> Void) -> Void {
+    if health.status.state == .UP {
+        respondWith(health.status, nil)
+    } else {
+        respondWith(nil, RequestError(.serviceUnavailable, body: health.status))
     }
-    
 }
