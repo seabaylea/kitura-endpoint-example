d982312ffb19d920b5a85cfb8df9c221f991bf10
Chris Bailey <baileyc@uk.ibm.com>
2019-01-07 17:43:48 +0000

    Update MyRoutes.swift

--- Sources/Application/Routes/MyRoutes.swift
+++ Sources/Application/Routes/MyRoutes.swift
@@ -4,7 +4,18 @@ import KituraContracts
 
 func initializeMyRoutes(app: App) {
     
-    app.router.get("/myroute") { (respondWith: (Status?, RequestError?) -> Void) -> Void in
+    app.router.get("/myroute", handler: getStatus)
+    app.router.get("/mysecureroute", handler: getStatusSecure)
+    
+    private func getStatus(respondWith: @escaping (Status?, RequestError?) -> Void) -> Void {
+        if health.status.state == .UP {
+            respondWith(health.status, nil)
+        } else {
+            respondWith(nil, RequestError(.serviceUnavailable, body: health.status))
+        }
+    }
+    
+    private func getStatus(user: UserAuth, respondWith: @escaping (Status?, RequestError?) -> Void) -> Void {
         if health.status.state == .UP {
             respondWith(health.status, nil)
         } else {
